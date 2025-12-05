d242fa1688de17ad16263402fad62f849d823c1e
Kevin De la Rosa <kevindlrm@MacBook-Air-de-Kevin.local>
2025-12-05 23:33:19 +0000

    Forzar redeploy para yt-dlp

--- server.js
+++ server.js
@@ -46,3 +46,4 @@ app.post('/audio-formats', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+// Redeploy forzado para arreglar yt-dlp
